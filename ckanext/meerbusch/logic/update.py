d68f478ee8bca89b11404a35bea5c1948a801bab
Nikola Demerdjiev <nikola.demerdjiev@keitaro.com>
2017-05-02 14:56:27 +0200

    Fix resource update functionality

--- ckanext/meerbusch/logic/update.py
+++ ckanext/meerbusch/logic/update.py
@@ -1,4 +1,3 @@
-import ast
 import logging
 import json
 from datetime import datetime
@@ -12,46 +11,52 @@ def package_update(context, data_dict):
     print "data_dict"
     print data_dict
 
-    temporal_coverage = json.loads(package_dict['temporal_coverage'])
-    temporal_from = temporal_coverage['temporal_coverage_from']
-    temporal_to = temporal_coverage['temporal_coverage_to']
-
-    try:
-        temporal_coverage['temporal_coverage_from'] = datetime.strptime(temporal_from, '%d.%m.%Y').strftime('%Y-%m-%d')
-        temporal_coverage['temporal_coverage_to'] = datetime.strptime(temporal_to, '%d.%m.%Y').strftime('%Y-%m-%d')
-    except ValueError:
-        temporal_coverage['temporal_coverage_from'] = temporal_from
-        temporal_coverage['temporal_coverage_to'] = temporal_to
     
-    dates = json.loads(package_dict['dates'])
     try:
-        for date_role in dates:
-            date_role['date'] = datetime.strptime(date_role['date'], '%d.%m.%Y').strftime('%Y-%m-%d')
-    except ValueError:
-        pass
-
-    data_dict['dates-1-date'] = dates[0]['date']
-    data_dict['dates-1-role'] = dates[0]['role']
-    data_dict['dates-2-date'] = dates[1]['date']
-    data_dict['dates-2-role'] = dates[1]['role']
-    data_dict['dates-3-date'] = dates[2]['date']
-    data_dict['dates-3-role'] = dates[2]['role']
-
-
-    # On resource_update the data_dict keys are as follows 'temporal_coverage-temporal_coverage_from/to'
+        temporal_coverage = json.loads(data_dict['temporal_coverage'])
+        temporal_from = temporal_coverage['temporal_coverage_from']
+        temporal_to = temporal_coverage['temporal_coverage_to']
+        try:
+            temporal_coverage['temporal_coverage_from'] = datetime.strptime(temporal_from, '%d.%m.%Y').strftime('%Y-%m-%d')
+            temporal_coverage['temporal_coverage_to'] = datetime.strptime(temporal_to, '%d.%m.%Y').strftime('%Y-%m-%d')
+        except ValueError:
+            temporal_coverage['temporal_coverage_from'] = temporal_from
+            temporal_coverage['temporal_coverage_to'] = temporal_to
+    except KeyError:
+        temporal_from = data_dict['temporal_coverage-temporal_coverage_from']
+        temporal_to = data_dict['temporal_coverage-temporal_coverage_to']
+        try:
+            data_dict['temporal_coverage-temporal_coverage_from'] = datetime.strptime(temporal_from, '%d.%m.%Y').strftime('%Y-%m-%d')
+            data_dict['temporal_coverage-temporal_coverage_to'] = datetime.strptime(temporal_to, '%d.%m.%Y').strftime('%Y-%m-%d')
+        except ValueError:
+            data_dict['temporal_coverage-temporal_coverage_from'] = temporal_from
+            data_dict['temporal_coverage-temporal_coverage_to'] = temporal_to
+    
     try:
-        data_dict['temporal_coverage-temporal_coverage_from'] = datetime.strptime(temporal_from, '%d.%m.%Y').strftime('%Y-%m-%d')
-        data_dict['temporal_coverage-temporal_coverage_to'] = datetime.strptime(temporal_to, '%d.%m.%Y').strftime('%Y-%m-%d')
-    except ValueError:
-        data_dict['temporal_coverage-temporal_coverage_from'] = temporal_from
-        data_dict['temporal_coverage-temporal_coverage_to'] = temporal_to
-     
+        dates = json.loads(data_dict['dates'])
+        try:
+            for date_role in dates:
+                date_role['date'] = datetime.strptime(date_role['date'], '%d.%m.%Y').strftime('%Y-%m-%d')
+        except ValueError:
+            for date_role in dates:
+                date_role['date'] = date_role['date']
+        data_dict['dates'] = dates
+    except KeyError:
+        try:
+            data_dict['dates-1-date'] = datetime.strptime(data_dict['dates-1-date'], '%d.%m.%Y').strftime('%Y-%m-%d')
+            data_dict['dates-2-date'] = datetime.strptime(data_dict['dates-2-date'], '%d.%m.%Y').strftime('%Y-%m-%d')
+            data_dict['dates-3-date'] = datetime.strptime(data_dict['dates-3-date'], '%d.%m.%Y').strftime('%Y-%m-%d')
+        except ValueError:
+            data_dict['dates-1-date'] = data_dict['dates-1-date']
+            data_dict['dates-2-date'] = data_dict['dates-2-date']
+            data_dict['dates-3-date'] = data_dict['dates-3-date']
 
-
+ 
     print 'after data_dict'
     print '-------------------------------------------------'
     print data_dict
     print type(data_dict)
+    print '-------------------------------------------------'
 
     
     dataset = l.action.update.package_update(context, data_dict)
